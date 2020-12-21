388a3c3127a6bd89244f5ec1b39d9f932ac5983b
hwangpaul <71800750+hwangpaul@users.noreply.github.com>
2020-12-21 15:26:29 +0900

    commit

---
+++ src/main/java/com/shop/md1/config/MessageLocaleConfig.java
@@ -0,0 +1,46 @@
+package com.shop.md1.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class MessageLocaleConfig implements WebMvcConfigurer{
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		
+		CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+		cookieLocaleResolver.setDefaultLocale(Locale.KOREAN);
+		cookieLocaleResolver.setCookieName("lang");
+		
+		return cookieLocaleResolver;
+	}
+	
+	//url 주소
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		
+		localeChangeInterceptor.setParamName("lang");
+		
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(localeChangeInterceptor())
+		.addPathPatterns("/**");
+
+		WebMvcConfigurer.super.addInterceptors(registry);
+	}
+	
+}
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%> 
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,8 +25,12 @@
 			<a href="./member/memberLogout">LOGOUT</a>
 			<a>MY PAGE</a>
 		</c:otherwise>
-	</c:choose>		
+	</c:choose>	
+	
+	
+		
 </div>
+<h1><spring:message code="hello.main"></spring:message></h1>
 
 <div class="container">contents</div>
 </body>
